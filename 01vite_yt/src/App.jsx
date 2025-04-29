78d561d4adf4d2c9b78929bbd9fd960331b2ad36
Bariharsh <144584767+Bariharsh@users.noreply.github.com>
2025-04-29 12:16:22 +0530

    all files saved

---
+++ 01vite_yt/src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+  const username = "Harsh"
+
+  return (
+    <>
+      <h1> {username} </h1>
+    </>
+  )
+}
+
+export default App
---
+++ 01vite_yt/src/main.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+function MyApp(){
+  return (
+    <div>
+      <h1>Custom App!</h1>
+    </div>
+  )
+}
+
+// const ReactElement = {
+//   type: 'a',
+//   props: {
+//       href: 'https://google.com',
+//       target: '_blank'
+//   },
+//   children: 'Click me to visit Google'
+// }
+
+const anotherElement = (
+  <a href="https://google.com" target='_blank'>Visit Google</a>
+)
+
+const anotherUser = "Harsh 2"
+
+const reactElement = React.createElement(
+  'a',
+  {href:'https://google.com', target: '_blank'},
+  'Click Me to Visit Google',
+  anotherUser
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    reactElement
+)
